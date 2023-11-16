5cc9d2d40803887abd49c04b59d6adac7b1aa13b
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-16 08:25:14 +0600

    Display All users and Create Make admin API and user delete api

--- index.js
+++ index.js
@@ -50,7 +50,27 @@ async function run() {
       res.send(result);
     });
 
-    //     get all menus from database
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin',
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    // menu related apis
+    // get all menus from database
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);

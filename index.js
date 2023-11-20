782b8b18becacf7fc66b7432ecf88d22440f54b6
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-20 11:31:03 +0600

    Save payment info and delete cart items in the database

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
     const userCollection = client.db('bistroDb').collection('users');
     const reviewsCollection = client.db('bistroDb').collection('reviews');
     const cartCollection = client.db('bistroDb').collection('carts');
+    const paymentCollection = client.db('bistroDb').collection('payments');
 
     // jwt related api
     app.post('/jwt', async (req, res) => {
@@ -219,6 +220,21 @@ async function run() {
       res.send({
         clientSecret: paymentIntent.client_secret,
       });
+    });
+
+    app.post('/payments', async (req, res) => {
+      const payment = req?.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      // carefully delete each item from the card
+      console.log('payment info', payment);
+      const query = {
+        _id: {
+          $in: payment?.cartIds.map((id) => new ObjectId(id)),
+        },
+      };
+      const deleteResult = await cartCollection.deleteMany(query);
+      res.send({ paymentResult, deleteResult });
     });
 
     // Send a ping to confirm a successful connection

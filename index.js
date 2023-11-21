ae2227029fe6f0d9603dabfd49fe0e6683d5b7d9
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-21 12:44:22 +0600

    stats api updated

--- index.js
+++ index.js
@@ -247,7 +247,7 @@ async function run() {
     });
 
     // stats or analytics
-    app.get('/admin-stats', async (req, res) => {
+    app.get('/admin-stats', verifyToken, verifyAdmin, async (req, res) => {
       const users = await userCollection.estimatedDocumentCount();
       const menuItems = await menuCollection.estimatedDocumentCount();
       const orders = await paymentCollection.estimatedDocumentCount();
@@ -259,7 +259,20 @@ async function run() {
       //   0
       // );
 
-      const result = await paymentCollection.aggregate([{}]);
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null,
+              totalRevenue: {
+                $sum: '$price',
+              },
+            },
+          },
+        ])
+        .toArray();
+
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
 
       res.send({
         users,
